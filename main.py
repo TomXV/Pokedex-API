514c960a65f06ee52e8e903d1f881c49d4274c95
Tom_XV <55909638+TomXV@users.noreply.github.com>
2022-01-31 21:53:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,14 +45,6 @@ async def pokedex(
         response = data["pokedex"][json_number]
     return response
 
-@app.get("/api/pokedex/{id}")
-def pokedex(id:int):
-    id -= 1
-    print(id)
-    with open('./pokedex/pokedex.json', 'rb') as f:
-        data = json.load(f)
-    return data["pokedex"][id]
-
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: str = None):
 #    return {"item_id": item_id, "q": q}
