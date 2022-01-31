fab667d050ecd0f0fc8f037581153ad403b7aa6c
Tom_XV <55909638+TomXV@users.noreply.github.com>
2022-01-31 18:46:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
 from plistlib import load
+import re
+from typing import Optional
 from fastapi import FastAPI
 import json
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api")
 def read_root():
-    return {"Hello": "World"}
+    return("Welcome to the Pokedex API!")
 
-@app.get("/api/pokedex/{id}")
-def pokedex(id:int):
-    id -= 1
-    print(id)
+@app.get("/api/pokedex/")
+def pokedex():
     with open('./pokedex/pokedex.json', 'rb') as f:
         data = json.load(f)
-    return data["pokedex"][id]
+    return data["pokedex"]
+
+@app.get("/api/pokedex/{json_number}")
+async def pokedex(
+    json_number :int,
+    id :Optional[bool] = None,
+    name :Optional[bool] = None,
+    classification :Optional[bool] = None,
+    height :Optional[bool] = None,
+    weight :Optional[float] = None
+    ):
+
+    json_number -= 1
+    print(f"request_id:{json_number + 1}")
+
+    with open('./pokedex/pokedex.json', 'rb') as f:
+        data = json.load(f)
+
+    if id is True:
+        response = data["pokedex"][json_number]["id"]
+
+    if name is True:
+        response = data["pokedex"][json_number]["name"]
+
+    if classification is True:
+        response = data["pokedex"][json_number]["classification"]
+
+    if height is True:
+        response = data["pokedex"][json_number]["height"]
+
+    if weight is True:
+        response = data["pokedex"][json_number]["weight"]
+
+    else:
+        response = data["pokedex"][json_number]
+    return response
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: str = None):
